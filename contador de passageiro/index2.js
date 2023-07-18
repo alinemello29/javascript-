8269d3c7212fcfaec128caca9d97379a208c75a5
Aline Mello <109696840+alinemello29@users.noreply.github.com>
2023-07-18 17:21:45 -0300

    Add files via upload

---
+++ contador de passageiro/index2.js
@@ -0,0 +1,11 @@
+/*document.getElementById("count").innerText=5  */
+
+let count=0
+console.log(count)
+console.log(myAge)
+
+/*1.Crie uma variável, myAge, e defina seu valor para sua idade  */
+let myAge=35
+
+/*2.Regsitre a variável myAage no console  */
+console.loge(myAge)
---
+++ contador de passageiro/index3.js
@@ -0,0 +1,21 @@
+/*document.getElementById("count").innertext=5*/
+
+/* Crie duas varáveis, myAge e humanDoRatio
+Multiplique os dois e armazena o resultado em myDoAge
+Log myDoAge no console*/
+
+let firstBatch=5
+let secondBatch=7
+
+let count= firstBatch + secondBatch
+
+console.log(count)
+
+/*cmd+k+c*/
+
+let myAge=35
+let humanDoRatio=7
+
+let myDoAge=myAge * humanDoRatio
+
+console.log(myDoAge)
